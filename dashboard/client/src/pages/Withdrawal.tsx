6f8c5ff1dfab31e346ed27f70b6583cb3c96760f
AmaraCharles <falsepegasus@gmail.com>
2026-03-03 13:44:32 +0100

    first commit

--- dashboard/client/src/pages/Withdrawal.tsx
+++ dashboard/client/src/pages/Withdrawal.tsx
@@ -44,7 +44,7 @@ export default function Withdrawal() {
 
     try {
       const response = await fetch(
-        `https://pruxde-render.onrender.com/transactions/${dataObj._id}/withdrawal`,
+        `https://puxde-render.onrender.com/transactions/${dataObj._id}/withdrawal`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
